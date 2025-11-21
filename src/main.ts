3614f76ad1fd03bf8484dc8237ac03328ceb5e03
Dennis Bejze <53354166+Bejdenn@users.noreply.github.com>
2025-11-21 16:42:59 +0100

    feat: overhaul & connect to emulator

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,42 @@
-import m, { type Vnode } from "mithril";
-import type { Album } from "./albums.ts";
-import { albums } from "./albums.ts";
-import "./style.css";
+import m, { type Component } from "mithril";
+import { collection, connectFirestoreEmulator, getDocs, getFirestore, Timestamp, type WithFieldValue } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
 
-type sortBy = "Title" | "Artist" | "Release" | "Rating";
+const app = initializeApp({
+  apiKey: "AIzaSyAiATvIjGzb1rikJsCmJyoz_GxzVaDUkZY",
+  authDomain: "soundscalz.firebaseapp.com",
+  projectId: "soundscalz",
+  storageBucket: "soundscalz.firebasestorage.app",
+  messagingSenderId: "1062045686996",
+  appId: "1:1062045686996:web:b47fa4e1db50ff343daf41"
+});
 
-type AlbumSortFn = Parameters<typeof albums.sort>[0]
+const db = getFirestore(app);
+if (import.meta.env.DEV) connectFirestoreEmulator(db, '127.0.0.1', 8089);
 
-const sortByArtist: AlbumSortFn = (a, b) => a.artists[0].localeCompare(b.artists[0])
-const sortByTitle: AlbumSortFn = (a, b) => a.title.localeCompare(b.title)
-const sortByDate: AlbumSortFn = (a, b) => b.releaseDate.getTime() - a.releaseDate.getTime()
-const sortByRating: AlbumSortFn = (a, b) => (b.rating || 0) - (a.rating || 0)
+type Album = {
+  title: string;
+  artists: string[];
+  releaseDate: Date;
+  rating?: number;
+};
 
-function AlbumRow() {
-  return {
-    view: function ({ attrs }: Vnode<Album>) {
-      return m("tr", { class: !attrs.rating ? "muted" : "" },
-        [
-          m("td", { align: "right" }, Stars(attrs.rating)),
-          m("td", attrs.artists.join(", ")),
-          m("td", m("i", attrs.title)),
-          m("td", { align: "center" }, attrs.releaseDate?.toLocaleDateString(undefined, { month: "short", year: "numeric" })),
-        ]);
-    },
-  };
+type compareFn = Parameters<typeof albums.sort>[0]
+
+const sortByArtist: compareFn = (a, b) => {
+  return a.artists[0].localeCompare(b.artists[0]);
+}
+
+const sortByTitle: compareFn = (a, b) => {
+  return a.title.localeCompare(b.title);
+}
+
+const sortByDate: compareFn = (a, b) => {
+  return b.releaseDate.getTime() - a.releaseDate.getTime();
+}
+
+const sortByRating: compareFn = (a, b) => {
+  return (b.rating || 0) - (a.rating || 0);
 }
 
 function Stars(n: number | undefined) {
@@ -34,40 +47,83 @@ function Stars(n: number | undefined) {
   return "★".repeat(n);
 }
 
-m.route(document.body, "/music", {
-  "/music": {
-    view: function () {
-      let sort: sortBy = m.route.param("sort") as sortBy || "Release";
+function AlbumRow(): Component<Album> {
+  return {
+    view: function ({ attrs: { rating, artists, title, releaseDate } }) {
+      return m("tr[style=word-break:keep-all]", { class: !rating ? "muted" : "" },
+        [
+          m("td[align=right]", Stars(rating)),
+          m("td", artists.join(", ")),
+          m("td", m("i", title)),
+          m("td[align=center]", releaseDate?.toLocaleDateString(undefined, { month: "short", year: "numeric" })),
+        ]);
+    },
+  };
+}
 
-      return m("main", { class: "smaller" }, [
-        m("h1", "My Music Reviews"),
-        m("form", { method: "get" }, [
-          m("label", [
-            "Sort by",
-            m("select", {
-              onchange: (e: any) => {
-                m.route.set('/music', { sort: e.target.value })
-              }, value: sort
-            }, [
-              m("option", "Title"),
-              m("option", "Artist"),
-              m("option", "Release"),
-              m("option", "Rating")
-            ]),
+const Page: Component<{ sort?: string, albums: Album[] }> = {
+  view: function ({ attrs: { sort, albums } }) {
+    if (!sort) sort = "Release"
+
+    return m("main.smaller", [
+      m("h1", "My Music Reviews"),
+      m("form", [
+        m("label", [
+          "Sort by",
+          m("select", {
+            onchange: (e: any) => {
+              m.route.set('/music', { sort: e.target.value });
+            }, value: sort
+          }, [
+            m("option", "Title"),
+            m("option", "Artist"),
+            m("option", "Release"),
+            m("option", "Rating")
           ]),
         ]),
-        m(
-          "table",
-          m("tbody", [
-            [...albums].sort(
-              sort === "Title" ? sortByTitle
-                : sort === "Artist" ? (a, b) => (sortByArtist(a, b) || sortByDate(a, b))
-                  : sort === "Release" ? sortByDate
-                    : sort === "Rating" ? (a, b) => (sortByRating(a, b) || sortByDate(a, b))
-                      : () => 0).map((alb) => m(AlbumRow, alb)),
-          ]),
-        ),
-      ]);
-    }
+      ]),
+      m(
+        "table",
+        m("tbody", [
+          [...albums].sort(
+            sort === "Title" ? sortByTitle
+              : sort === "Artist" ? (a, b) => (sortByArtist(a, b) || sortByDate(a, b))
+                : sort === "Release" ? sortByDate
+                  : sort === "Rating" ? (a, b) => (sortByRating(a, b) || sortByDate(a, b))
+                    : () => 0).map((alb) => m(AlbumRow, alb)),
+        ])
+      ),
+    ]);
+  }
+};
+
+let albums: Album[];
+
+m.route(document.body, "/music", {
+  "/music": {
+    onmatch: async function () {
+      type AlbumDbType = {
+        title: string;
+        artists: string[];
+        rating?: number;
+        releaseDate: Timestamp;
+      };
+
+      const querySnapshot = await getDocs(collection(db, "albums").withConverter<Album, AlbumDbType>({
+        toFirestore: ({ title, artists, rating, releaseDate }: WithFieldValue<Album>) => {
+          return { title, artists, rating, releaseDate: Timestamp.fromDate(releaseDate as Date) };
+        },
+        fromFirestore: (snapshot) => {
+          const { title, artists, releaseDate, rating } = snapshot.data() as AlbumDbType;
+
+          return {
+            title, artists, releaseDate: releaseDate.toDate(), rating
+          };
+        }
+      }))
+
+      albums = querySnapshot.docs.map(doc => doc.data())
+    },
+    render: (vnode) => m(Page, { ...vnode.attrs, albums })
   },
 });
