9749f96aee598e3a42d04f7c3c54086f61824bee
Dennis Bejze <53354166+Bejdenn@users.noreply.github.com>
2025-11-22 12:08:24 +0100

    feat: add button to scroll to a random unrated album

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import m, { type Component } from "mithril";
 import { collection, connectFirestoreEmulator, getDocs, getFirestore, Timestamp, type WithFieldValue } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 
+import "./style.css"
+
 const app = initializeApp({
   apiKey: "AIzaSyAiATvIjGzb1rikJsCmJyoz_GxzVaDUkZY",
   authDomain: "soundscalz.firebaseapp.com",
@@ -15,10 +17,12 @@ const db = getFirestore(app);
 if (import.meta.env.DEV) connectFirestoreEmulator(db, '127.0.0.1', 8089);
 
 type Album = {
+  id?: string;
   title: string;
   artists: string[];
   releaseDate: Date;
   rating?: number;
+  highlight?: boolean;
 };
 
 type compareFn = Parameters<typeof albums.sort>[0]
@@ -49,8 +53,12 @@ function Stars(n: number | undefined) {
 
 function AlbumRow(): Component<Album> {
   return {
-    view: function ({ attrs: { rating, artists, title, releaseDate } }) {
-      return m("tr[style=word-break:keep-all]", { class: !rating ? "muted" : "" },
+    view: function ({ attrs: { id, rating, artists, title, releaseDate, highlight } }) {
+      let classes: string[] = [];
+      if (!rating) { classes.push("muted") };
+      if (highlight) { classes.push("accent"); };
+
+      return m("tr", { class: classes.join(" "), id, key: id },
         [
           m("td[align=right]", Stars(rating)),
           m("td", artists.join(", ")),
@@ -61,8 +69,8 @@ function AlbumRow(): Component<Album> {
   };
 }
 
-const Page: Component<{ sort?: string, albums: Album[] }> = {
-  view: function ({ attrs: { sort, albums } }) {
+const Page: Component<{ sort?: string }> = {
+  view: function ({ attrs: { sort } }) {
     if (!sort) sort = "Release"
 
     return m("main.smaller", [
@@ -81,6 +89,23 @@ const Page: Component<{ sort?: string, albums: Album[] }> = {
             m("option", "Rating")
           ]),
         ]),
+        m("button", {
+          onclick: function () {
+            let album = albums.find(album => album.highlight);
+            if (album) { album.highlight = !album.highlight; }
+
+            const id = albums.filter(album => !!!album.rating).map(album => album.id)[Math.floor(Math.random() * albums.length)];
+            album = albums.find(album => album.id === id);
+            if (!album) return;
+
+            album.highlight = true;
+
+            albums[albums.findIndex(album => album.id === id)] = album;
+
+            const element = document.querySelector(`#${id}`);
+            element?.scrollIntoView();
+          }
+        }, "Go to random unrated album")
       ]),
       m(
         "table",
@@ -95,6 +120,7 @@ const Page: Component<{ sort?: string, albums: Album[] }> = {
       ),
     ]);
   }
+
 };
 
 let albums: Album[];
@@ -102,6 +128,11 @@ let albums: Album[];
 m.route(document.body, "/music", {
   "/music": {
     onmatch: async function () {
+      // 'onmatch' runs everytime there is a change in the route, for example when we append the sorting key
+      // to the URL. In Safari, for some reason, it runs a second time, even when the route does NOT change, as
+      // with clicking the button to show a random album.
+      if (albums) return
+
       type AlbumDbType = {
         title: string;
         artists: string[];
@@ -122,8 +153,8 @@ m.route(document.body, "/music", {
         }
       }))
 
-      albums = querySnapshot.docs.map(doc => doc.data())
+      albums = querySnapshot.docs.map(doc => ({ ...doc.data(), id: `id-${doc.id}` }))
     },
-    render: (vnode) => m(Page, { ...vnode.attrs, albums })
+    render: (vnode) => m(Page, vnode.attrs)
   },
 });
