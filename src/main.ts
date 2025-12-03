884ca1321ec51c9a42be88726caec7b89ecafe75
Dennis Bejze <53354166+Bejdenn@users.noreply.github.com>
2025-12-03 15:22:51 +0100

    current state of migration to tailwind

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import m, { type Component } from "mithril";
 import { collection, getDocs, getFirestore, Timestamp, type WithFieldValue } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 
-import "./style.css"
 import { Cache, Observed } from "./utils";
 
 const app = initializeApp({
@@ -68,16 +67,21 @@ function AlbumRow(): Component<Album> {
   return {
     view: function ({ attrs: { id, rating, artists, title, releaseDate, currentlyListening } }) {
       const classes: string[] = [];
-      if (rating) { classes.push("is-dark") };
-      if (highlighted.get()?.id === id) { classes.push("is-selected"); };
+      if (rating) { classes.push("bg-neutral-700 text-white"); };
+      if (highlighted.get()?.id === id) { classes.push("bg-indigo-300", "text-black"); };
 
       return m("tr", { class: classes.join(" "), id, key: id },
-        [
-          m("td[align=right]", rating ? m("span", Stars(rating)) : currentlyListening ? m(MusicNote, { id: id! }) : null),
-          m("td", artists.join(", ")),
-          m("td", m("i", title)),
-          m("td[align=right][style=white-space:nowrap]", releaseDate?.toLocaleDateString(undefined, { month: "short", year: "numeric" })),
-        ]);
+
+        ([
+          { align: "right", value: rating ? m("span", Stars(rating)) : currentlyListening ? m(MusicNote, { id: id! }) : null },
+          { value: artists.join(", ") },
+          { value: title },
+          { value: releaseDate?.toLocaleDateString(undefined, { month: "short", year: "numeric" }) }
+        ]).map(cell => m("td", {
+          align: cell.align ?? "left",
+          class: "border-b border-gray-100 p-2 pl-8 dark:border-gray-700"
+        }, cell.value))
+      );
     },
   };
 }
@@ -151,39 +155,44 @@ const sortAlbums = (albums: Album[], sort: SortBy) => {
 
 const Page: Component<{ sort: SortBy }> = {
   view: function ({ attrs: { sort } }) {
-    return m("section.section",
-      m("main.container", [
-        m("h1.title.is-1", "My Music Reviews"),
-        m(".columns .is-mobile .is-vcentered",
-          m(".column",
-            m(".field", [
-              m("label.label", "Sort by"),
-              m(".select",
-                m("select", {
-                  onchange: (e: Event) => {
-                    const sort = (e.target as HTMLSelectElement).value;
-                    if (isSortBy(sort)) albums = sortAlbums(albums, sort);
+    return m("section", { class: "lg:px-24 lg:py-14" },
+      m("main", [
+        m("h1", { class: "text-5xl dark:text-white font-extrabold" }, "My Music Reviews"),
+        m("div", { class: "mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6" },
+          m("div", { class: "sm:col-span-3" }, [
+            m("label", { class: "block text-sm/6 font-medium text-black dark:text-white" }, "Sort by"),
+            m("div", { class: "mt-2 grid grid-cols-1" }, [
+              m("select", {
+                class: "col-start-1 row-start-1 w-full appearance-none rounded-md bg-black/5 dark:bg-white/5 py-2 pr-8 pl-3 text-base text-black dark:text-white outline-1 -outline-offset-1 outline-black/10 dark:outline-white/10 *:bg-gray:800 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6",
+                id: "sort",
+                name: "sort",
+                onchange: (e: Event) => {
+                  const sort = (e.target as HTMLSelectElement).value;
+                  if (isSortBy(sort)) albums = sortAlbums(albums, sort);
 
-                    m.route.set('/music', { sort });
-                  }, value: sort
-                }, [
-                  (["Title", "Artist", "Release", "Rating", "Listening"] satisfies SortBy[])
-                    .map(sb => m("option", sb))
-                ]))
-            ]),
-          ),
+                  m.route.set('/music', { sort });
+                }, value: sort
+              }, [
+                (["Title", "Artist", "Release", "Rating", "Listening"] satisfies SortBy[])
+                  .map(sb => m("option", sb))
+              ]),
+              m("svg", { class: "pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-400 sm:size-4", viewBox: "0 0 16 16", fill: "currentColor", "data-slot": "icon", "aria-hidden": "true" },
+                m("path[d=M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z][clip-rule=evenodd][fill-rule=evenodd]")
+              )
+            ])
+          ])
         ),
-        !isMobile ? m(".table-container",
-          m("table.table.is-striped",
-            m("tbody", [
+        !isMobile ? m("div", { class: "mt-10 not-prose overflow-auto rounded-lg bg-white outline outline-white/5 dark:bg-gray-950/50" },
+          m("table", { class: "w-full table-auto border-collapse text-sm" },
+            m("tbody", { class: "bg-inherit dark:text-white" }, [
               albums.map((alb) => m(AlbumRow, alb)),
             ])
           )
         ) :
           albums.map((alb) => m(AlbumCard, alb))
       ]),
-      m("button.button.is-light", {
-        style: { position: "fixed", bottom: "1rem", right: "1rem", "z-index": "10", boxShadow: "0px 2px 6px 0px rgba(0, 0, 0, 0.3), 0px 8px 16px 0px rgba(0, 0, 0, 0.5)" },
+      m("button", {
+        class: "fixed bottom-4 right-4 z-10 shadow-md/30 py-2.25 px-2.5 rounded-md bg-white hover:bg-slate-100 active:bg-slate-200",
         onclick: function () {
           const id = albums.filter(album => !album.rating).map(album => album.id)[Math.floor(Math.random() * albums.length)];
           const album = albums.find(album => album.id === id);
@@ -204,7 +213,7 @@ const Page: Component<{ sort: SortBy }> = {
 let albums: Album[];
 let highlighted: Observed<Album>;
 
-m.route(document.body, "/music", {
+m.route(document.querySelector<HTMLDivElement>('#app')!, "/music", {
   "/music": {
     onmatch: async function (args: { sort?: string }) {
       // 'onmatch' runs everytime there is a change in the route, for example when we append the sorting key
